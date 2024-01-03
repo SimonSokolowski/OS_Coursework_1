398bedf19256c5f8cf85d1dcd8dd87896c4768df
Simon <simon.sokolowski@outlook.com>
2024-01-03 15:25:20 +0000

    remove std to increase speed

--- main.cpp
+++ main.cpp
@@ -2,65 +2,63 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 int main()
 {
     int P, R;
-    cout << "Enter number of processes: ";
-    cin >> P;
+    std::cout << "Enter number of processes: ";
+    std::cin >> P;
 
-    cout << "Enter number of resources: ";
-    cin >> R;
+    std::cout << "Enter number of resources: ";
+    std::cin >> R;
 
     BankersAlgorithm bankers(P, R);
 
-    vector<int> avail(R);
-    cout << "Enter available resources (format: x y z ...): ";
+    std::vector<int> avail(R);
+    std::cout << "Enter available resources (format: x y z ...): ";
     for (int &a : avail)
-        cin >> a;
+        std::cin >> a;
     bankers.setAvailable(avail);
 
     for (int i = 0; i < P; i++)
     {
-        vector<int> maxReq(R), allocReq(R);
+        std::vector<int> maxReq(R), allocReq(R);
 
-        cout << "Enter maximum resource request for Process " << i << " (format: x y z ...): ";
+        std::cout << "Enter maximum resource request for Process " << i << " (format: x y z ...): ";
         for (int &m : maxReq)
-            cin >> m;
+            std::cin >> m;
         bankers.setMaximum(i, maxReq);
 
-        cout << "Enter number of resources allocated to Process " << i << " (format: x y z ...): ";
+        std::cout << "Enter number of resources allocated to Process " << i << " (format: x y z ...): ";
         for (int &a : allocReq)
-            cin >> a;
+            std::cin >> a;
         bankers.setAllocation(i, allocReq);
     }
 
     bankers.isSafe();
 
-    vector<int> request(R);
+    std::vector<int> request(R);
     int processId;
     bool continueRequesting = true; // Flag to control the loop
 
     while (continueRequesting)
     {
-        cout << "Enter Process ID for resource request: ";
-        cin >> processId;
+        std::cout << "Enter Process ID for resource request: ";
+        std::cin >> processId;
 
         if (processId < 0 || processId >= P)
         {
-            cout << "Invalid Process ID. Please enter a valid Process ID (0 to " << P - 1 << ")." << endl;
+            std::cout << "Invalid Process ID. Please enter a valid Process ID (0 to " << P - 1 << ")." << std::endl;
             continue; // Skip to the next iteration of the loop
         }
 
-        cout << "Enter resource request (format: x y z ...): ";
+        std::cout << "Enter resource request (format: x y z ...): ";
         for (int &r : request)
-            cin >> r;
+            std::cin >> r;
 
         bankers.requestResources(processId, request);
 
-        cout << "Continue requesting resources? (1 for yes, 0 for no): ";
-        cin >> continueRequesting;
+        std::cout << "Continue requesting resources? (1 for yes, 0 for no): ";
+        std::cin >> continueRequesting;
     }
 
     return 0;
